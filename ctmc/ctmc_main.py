312ae703d767bee8799cbdc8bb39b1093ff943ed
Alexander Moreno <alexander.f.moreno@gatech.edu>
2017-07-15 22:24:00 -0400

    skeleton CTMC done

--- ctmc/ctmc_main.py
+++ ctmc/ctmc_main.py
@@ -4,6 +4,21 @@ from patient import Patient
 from ctmc_expectations import Eigen_Nij_all_times, Eigen_TauI_all_times
 
 
+def EM_step(Q, patient, globalParams, pi0):
+    num_state = globalParams['numStates']
+    chain = patient.O
+    Nij = Eigen_Nij_all_times(Q, patient, chain,globalParams,pi0)
+    TauI = Eigen_TauI_all_times(Q, patient,chain, globalParams,pi0)
+    for i in range(num_state):
+        for j in range(num_state):
+            if i!=j:
+                Q[i,j] = Nij[i,j]/TauI[i]
+    for i in range(num_state):
+        Q[i,i] = 0
+        Q[i,i] = -np.sum(Q[i,:])
+    return Q
+
+
 if __name__=='__main__':
     Q = np.array([[-1., 0.3, 0.7], [1.,-2., 1.], [1, 3, -4]])
     Q_true = Q
@@ -17,11 +32,12 @@ if __name__=='__main__':
     Q = np.array([[-rate1, rate1/2, rate1/2], [rate2/3,-rate2, 2*rate2/3], [2*rate3/4,2*rate3/4, -rate3]])
     globalParams['Q'] = Q
     globalParams['numStates'] = 3
-    T_max = 5
+    T_max = 5000
 
     patient = Patient()
     patient.initialize_randomly(Q_true,globalParams,T_max,pi0)
-    print patient.latent_trajectory
-    print patient.T_latent
-    print patient.observation_times
-    print patient.O
+
+    for i in range(50):
+        globalParams['Q'] = EM_step(globalParams['Q'],patient,globalParams,pi0)
+        print Q_true
+        print globalParams['Q']
