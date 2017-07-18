7a4a2edb8aa3bf6dc7076c07093f97f3c4f91dbf
Soujanya27 <soujanya.chatterjee@gmail.com>
2017-07-18 15:53:08 -0500

    Updated import and the I_matrix issue

--- ctmc/patient.py
+++ ctmc/patient.py
@@ -5,12 +5,12 @@ import pandas as pd
 
 
 class Patient:
-    def initialize_randomly(self,Q,globalParams,T_max,pi0):
-        self.generate_latent_trajectory(Q,T_max,pi0)
-        self.generate_observation_times(1,T_max)
+    def initialize_randomly(self, Q, globalParams, T_max, pi0):
+        self.generate_latent_trajectory(Q, T_max, pi0)
+        self.generate_observation_times(1, T_max)
         self.get_observation_trajectory()
-    
-    def generate_latent_trajectory(self,Q,T_max,pi0):
+
+    def generate_latent_trajectory(self, Q, T_max, pi0):
         '''
         @summary: use transition rate matrix Q to generate latent trajectory
         @param Q: transition matrix
@@ -20,35 +20,35 @@ class Patient:
         state = self.discrete_sampler(pi0)
         t = 0.0
         trajectory.append([state, t])
-        U = np.random.uniform(0,1)
+        U = np.random.uniform(0, 1)
         covariates = []
-        while t<T_max:
-            #get the rate
-            rate = -Q[state,state]
-            covariates.append(np.array([state+1.]))
+        while t < T_max:
+            # get the rate
+            rate = -Q[state, state]
+            covariates.append(np.array([state + 1.]))
             hazard = 1.
-            #get next jump time
-            jump_interval = np.random.exponential(1./rate)
-            increment = jump_interval*hazard
-            #get new state
-            #if t<T_max, get row, divide all non-diagonal elements by the rate
-            p_vector = Q[state,:]/rate
-            #add the rate to the diagonal element to get a 0
-            p_vector[state]+=1
-            #sample from this vector to get the new state
+            # get next jump time
+            jump_interval = np.random.exponential(1. / rate)
+            increment = jump_interval * hazard
+            # get new state
+            # if t<T_max, get row, divide all non-diagonal elements by the rate
+            p_vector = Q[state, :] / rate
+            # add the rate to the diagonal element to get a 0
+            p_vector[state] += 1
+            # sample from this vector to get the new state
             state = self.discrete_sampler(p_vector)
             t += jump_interval
-            #append this to the trajectory
-            trajectory.append([state,t])
+            # append this to the trajectory
+            trajectory.append([state, t])
         self.latent_trajectory = np.array(trajectory)
         self.T_latent = np.shape(self.latent_trajectory)[0]
 
-    def generate_observation_times(self,rate,T_max):
+    def generate_observation_times(self, rate, T_max):
         t = 0
         T = []
         T.append(t)
         while t < T_max:
-            t = t+np.random.exponential(1./rate)
+            t = t + np.random.exponential(1. / rate)
             if t < T_max:
                 T.append(t)
         self.observation_times = np.array(T)
@@ -57,20 +57,19 @@ class Patient:
     def get_observation_trajectory(self):
         O = []
         for t in self.observation_times:
-            ind = np.searchsorted(self.latent_trajectory[:,1],t)
-            O.append(self.latent_trajectory[ind-1,0])
+            ind = np.searchsorted(self.latent_trajectory[:, 1], t)
+            O.append(self.latent_trajectory[ind - 1, 0])
         self.O = np.array(O)
-            
 
-    def discrete_sampler(self,pi0):
-        val = np.random.uniform(0,1)
+    def discrete_sampler(self, pi0):
+        val = np.random.uniform(0, 1)
         total_prob = 0
         for i in range(len(pi0)):
             total_prob += pi0[i]
             if val < total_prob:
                 return i
 
-    def get_zeta(self,t,alpha,beta, globalParams,observations_t):
+    def get_zeta(self, t, alpha, beta, globalParams, observations_t):
         '''
             @summary: calculate zeta from the paper
             @param i,j: state from and to
@@ -83,6 +82,5 @@ class Patient:
             '''
         Q = globalParams['Q']
         b = self.b_s(observations_t)
-        likelihood = np.dot(alpha,np.dot(expm(Q*t),beta*b))
-        return expm(Q*t)*np.outer(alpha, np.transpose(b*beta))/likelihood
-
+        likelihood = np.dot(alpha, np.dot(expm(Q * t), beta * b))
+        return expm(Q * t) * np.outer(alpha, np.transpose(b * beta)) / likelihood
